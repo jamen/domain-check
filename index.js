61e9975d5a7fad344ec6af432660d85c1369bbc3
Jamen Marz <jamen.marz@gmail.com>
2015-10-26 20:52:00 -0600

    Add library

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+const lib = require('lib');
 if (process.stdout.isTTY) {
   // Being used as CLI.
+
 } else {
   // Being used as a module.
+  module.exports = exports = lib.check;
 }
