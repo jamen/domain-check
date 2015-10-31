8964af4c855c920c5bfb50e5034db92fd56d0ca4
Jamen Marz <jamen.marz@gmail.com>
2015-10-30 21:36:36 -0600

    Added file reading and parsing.

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 #!/usr/bin/env node
-const lib = require('./lib');
+'use strict';
+
+const lib = require('./lib'),
+      check = lib.check;
+
 if (process.stdout.isTTY) {
   // Being used as CLI.
-  
+
+  let cli = lib.args(process.argv),
+      domains = cli.domains,
+      options = cli.options;
+
+  if (options.indexOf('file') !== -1) {
+    const fs = require('fs'),
+          path = require('path');
+
+    let files = domains,
+        matcher = /,?(?:\r\n|\r|\n|\s)/,
+        domains = [];
+
+    files.forEach(function(file){
+      file = path.resolve(file);
+      try {
+        domains = domains.concat(fs.readFileSync(file).toString().split(matcher));
+      } catch (e) {}
+    });
+    if (domains[domains.length-1] === '') domains = domains.slice(0, -1);
+
+  } else {
+
+  }
 } else {
   // Being used as a module.
-  module.exports = exports = lib.check;
+  module.exports = exports = check;
 }
