553e14e226f0a331cebe09074baa3f7b657d8dda
Jamen Marz <jamen.marz@gmail.com>
2015-10-30 23:28:13 -0600

    Add check functionality and file reading

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ if (process.stdout.isTTY) {
   // Being used as CLI.
 
   let cli = lib.args(process.argv),
-      domains = cli.domains,
       options = cli.options;
 
   if (options.indexOf('file') !== -1) {
     const fs = require('fs'),
           path = require('path');
 
-    let files = domains,
+    let files = cli.domains,
         matcher = /,?(?:\r\n|\r|\n|\s)/,
         domains = [];
 
@@ -27,6 +26,38 @@ if (process.stdout.isTTY) {
     });
     if (domains[domains.length-1] === '') domains = domains.slice(0, -1);
 
+    check(domains, function(status){
+      if (options.indexOf('sort') !== -1) {
+        let available = [], taken = [];
+        for (let host in status) {
+          if (status[host]) available.push(host);
+          else taken.push(host);
+        }
+        console.log('available:');
+        console.log(available.join('\r\n'), '\r\n');
+        console.log('taken:');
+        console.log(taken.join('\r\n'));
+        return;
+      }
+
+      if (options.indexOf('available') !== -1) {
+        for (let host in status) {
+          if (status[host]) console.log(host);
+        }
+        return;
+      }
+
+      if (options.indexOf('taken') !== -1) {
+        for (let host in status) {
+          if (!status[host]) console.log(host);
+        }
+        return;
+      }
+
+      for (let host in status) {
+        console.log(host + ': ' + (status[host] ? 'available' : 'taken'));
+      }
+    });
   } else {
 
   }
